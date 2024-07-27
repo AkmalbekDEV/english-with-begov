609a60772970d668c83c0980a924827c48a3dbd9
AkmalbekDEV <akmalyusufov32@gmail.com>
2024-07-27 16:33:04 +0500

    change

--- src/components/Teachers.jsx
+++ src/components/Teachers.jsx
@@ -10,7 +10,7 @@ const Teachers = () => {
         <section id='teachers' className='grid gap-10 mt-5 mb-20'>
             <div className='flex max-sm:flex-col-reverse gap-16 max-sm:gap-10 items-center px-8 py-5 max-sm:px-5 max-sm:py-5'>
                 <div className='grid content-between gap-10 max-sm:gap-5 max-sm:text-center'>
-                    <h1 className='text-4xl font-bold'>Hi, I'm <span className='text-blue-700'>Begov Dostonbek</span></h1>
+                    <h1 className='text-4xl font-bold'>Hi, I'm <span className='text-blue-700'>Begov</span></h1>
                     <p className='text-lg font-medium text-gray-500'>
                         Men 2018 yil avgust oyida Xitoyning Shanghai shahrida joylashgan CTP Teaching Centre o'quv markazida ilk ish faoliyatimni boshlaganman va 2020 yil yanvarigacha u yerda ishlaganman. <div className='my-2'></div> Keyin esa O'zbekistonga qaytib, Urganch shahrida o'z o'quv kurslarimni ochdim va shu paytgacha 3000 dan ortiq o'quvchiga dars berib ularni yaxshi natijalarga erishishida yordam berib kelmoqdaman. <div className='my-2'></div> Bizning kurslarimizda zamonaviy o'qitish uslublari qo'llaniladi va har bir talabaning individual ehtiyojlariga e'tibor qaratiladi. O'quvchilarimiz qisqa vaqt ichida sezilarli natijalarga erishishadi va ingliz tilini erkin suhbatlashish darajasida o'rganishadi.
                     </p>
